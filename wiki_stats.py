b30793f79f06ea8d2901dd3fa320caf8adc9fac3
MargoGV <MV.Gubina@yandex.ru>
2016-03-28 11:25:00 +0400

    Update wiki_stats.py

--- wiki_stats.py
+++ wiki_stats.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python3
-
 import os
 import sys
 import math
@@ -14,62 +12,79 @@ rc('font', family='Droid Sans', weight='normal', size=14)
 import matplotlib.pyplot as plt
 
 
+
 class WikiGraph:
 
-    def load_from_file(self, filename):
-        print('Загружаю граф из файла: ' + filename)
-
-        with open(filename) as f:
-            (n, _nlinks) = (0, 0) # TODO: прочитать из файла
-            
-            self._titles = []
-            self._sizes = array.array('L', [0]*n)
-            self._links = array.array('L', [0]*_nlinks)
-            self._redirect = array.array('B', [0]*n)
-            self._offset = array.array('L', [0]*(n+1))
-
-            # TODO: прочитать граф из файла
-
-        print('Граф загружен')
-
-    def get_number_of_links_from(self, _id):
-        pass
-
-    def get_links_from(self, _id):
-        pass
-
-    def get_id(self, title):
-        pass
-
-    def get_number_of_pages(self):
-        pass
-
-    def is_redirect(self, _id):
-        pass
-
-    def get_title(self, _id):
-        pass
-
-    def get_page_size(self, _id):
-        pass
+	def load_from_file(self, filename):
+		print('Загружаю граф из файла' + filename)
+		with open(filename) as f:
+			(n, _nlinks) = (map(int, f.readline().split()))
+			self._titles = []
+			steps = 2854434 / 6  
+			self._sizes = array.array('L', [0]*n)
+			self._links = array.array('L', [0]*_nlinks)
+			self._redirect = array.array('B', [0]*n)
+			self._offset = array.array('L', [0]*(n+1))
+			n_ls = 0 
+			for i in range(n):
+				self._titles.append(f.readline().rstrip())
+				(size, redirect, ls) = (map(int, f.readline().split()))
+				self._sizes[i] = size
+				self._redirect[i] = redirect
+				for j in range(n_ls, n_ls + ls):
+					self._links[j] = int(str(f.readline()))
+				n_ls += ls
+				self._offset[i+1] = self._offset[i] + ls
+				if i % steps == 0:
+					print ('*', end = ' ')
+			print('Done!')
+		print('Граф загружен')
 
 
-def hist(fname, data, bins, xlabel, ylabel, title, facecolor='green', alpha=0.5, transparent=True, **kwargs):
-    plt.clf()
-    # TODO: нарисовать гистограмму и сохранить в файл
+	def get_number_of_links_from(self, _id):
+		return len(self._links[self._offset[_id]:self._offset[_id+1]])
+
+	def get_links_from(self, _id):
+		return self._links[self._offset[_id]:self._offset[_id+1]]
+
+	def get_id(self, title):
+		for i in range(len(self._titles)):
+			if self._titles[i] == title:
+				return(i)
+
+
+	def get_number_of_pages(self):
+		return len(self._titles)
+
+	def is_redirect(self, _id):
+		return self._redirect[_id]
+
+	def get_title(self, _id):
+		return self._titles[_id]
+
+	def get_page_size(self, _id):
+		return self._sizes[_id]
+
+
+	def hist(fname, data, bins, xlabel, ylabel, title, facecolor='green', alpha=0.5, transparent=True, **kwargs):
+		plt.clf()
+G=WikiGraph()    
+G.load_from_file('wiki_small.txt')        
+
+
 
 
 if __name__ == '__main__':
 
-    if len(sys.argv) != 2:
-        print('Использование: wiki_stats.py <файл с графом статей>')
-        sys.exit(-1)
+	if len(sys.argv) != 2:
+		print('Использование: wiki_stats.py <файл с графом статей>')
+		sys.exit(-1)
 
-    if os.path.isfile(sys.argv[1]):
-        wg = WikiGraph()
-        wg.load_from_file(sys.argv[1])
-    else:
-        print('Файл с графом не найден')
-        sys.exit(-1)
+	if os.path.isfile(sys.argv[1]):
+		wg = WikiGraph()
+		wg.load_from_file(sys.argv[1])
+	else:
+		print('Файл с графом не найден')
+		sys.exit(-1)
 
     # TODO: статистика и гистограммы
